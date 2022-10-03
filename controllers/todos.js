fb9b9fceae13db5f6f329c191b23a7b0655f57f5
Jim Clark <jim.clark@generalassemb.ly>
2022-10-03 11:13:35 -0700

    Finish partials lesson

--- controllers/todos.js
+++ controllers/todos.js
@@ -7,12 +7,14 @@ module.exports = {
 
 function show(req, res) {
   res.render('todos/show', {
-    todo: Todo.getOne(req.params.id)
+    todo: Todo.getOne(req.params.id),
+    title: 'To-Do Details'
   });
 }
 
 function index(req, res) {
   res.render('todos/index', {
-    todos: Todo.getAll()
+    todos: Todo.getAll(),
+    title: 'All To-Dos'
   });
 }
